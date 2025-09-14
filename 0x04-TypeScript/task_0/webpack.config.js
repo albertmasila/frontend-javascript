46fd19ef8213e9dc5fbed9668a7d9f6384e23a85
albertmasila <albertmasila4@gmail.com>
2025-09-14 23:15:13 +0300

    task_1

--- 0x04-TypeScript/task_0/webpack.config.js
+++ 0x04-TypeScript/task_0/webpack.config.js
@@ -1,29 +1,37 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
   entry: "./js/main.ts",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
+      }
+    ]
   },
   resolve: {
-    extensions: [".ts", ".js"],
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    extensions: [".tsx", ".ts", ".js"]
   },
   devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
-    compress: true,
-    port: 9000,
+    contentBase: "./dist"
   },
-  mode: "development",
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Development"
+    })
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
 };
